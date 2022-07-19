79886bfcfd5c72933c6ba2d73cac64d5eaea5ef6
Thormund <e0308991@u.nus.edu>
2022-07-19 17:42:54 +0800

    Remove unneccessary imports in BaseScript

--- ScriptLibrary/BaseScript.py
+++ ScriptLibrary/BaseScript.py
@@ -11,6 +11,7 @@
 
 # Initialiastion: Directory appending for my system. Vary the directories as necessary.
 import sys, os.path
+<<<<<<< HEAD
 # Add the FYP folder for import
 if 'FYPLibrary' not in [os.path.basename(x) for x in sys.path]:
     cur_path = os.path.dirname(__file__)
@@ -20,6 +21,12 @@ if 'FYPLibrary' not in [os.path.basename(x) for x in sys.path]:
             raise ValueError('Recursively climbed up directory past root!')
     sys.path.append(os.path.join(cur_path, 'FYPLibrary'))
     del cur_path
+=======
+
+# Add the FYPLibrary folder in the same level as this folder.
+if os.path.dirname(os.path.dirname(__file__)) not in sys.path:
+    sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'FYPLibrary'))
+>>>>>>> cb2c45f (Remove unneccessary imports in BaseScript)
 
 # from pprint import pprint
 # pprint(sys.path)
